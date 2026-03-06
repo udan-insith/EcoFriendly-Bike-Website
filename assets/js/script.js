90a78df3ed9570cd0fa4ecf65a150ab7818c1102
udan-insith <udaninsith23613@gmail.com>
2026-03-06 15:30:47 +0530

    navbar completed

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,3 +4,15 @@ const loadElement = document.querySelector("[data-preloader]")
 window.addEventListener("load", () => {
     loadElement.classList.add("loaded")
 })
+
+// Mobile navbar
+
+const navbar = document.querySelector("[data-navbar]")
+const navToggler = document.querySelector("[data-nav-toggler]")
+
+const toggleNavbar = function () {
+    navbar.classList.toggle("active")
+    navToggler.classList.toggle("active")
+}
+
+navToggler.addEventListener("click", toggleNavbar)
